f0eb9ca57c0e12bacad533fff934fd5ac735b0c5
sshulga <sergii.shulga@infopulse.com>
2015-10-07 10:24:02 +0300

    Initial Commit

---
+++ FunctionalMarvel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FunctionalMarvel
+//
+//  Created by Segii Shulga on 10/7/15.
+//  Copyright © 2015 Sergey Shulga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view, typically from a nib.
+   }
+
+   override func didReceiveMemoryWarning() {
+      super.didReceiveMemoryWarning()
+      // Dispose of any resources that can be recreated.
+   }
+
+
+}
+
---
+++ FunctionalMarvelTests/FunctionalMarvelTests.swift
@@ -0,0 +1,36 @@
+//
+//  FunctionalMarvelTests.swift
+//  FunctionalMarvelTests
+//
+//  Created by Segii Shulga on 10/7/15.
+//  Copyright © 2015 Sergey Shulga. All rights reserved.
+//
+
+import XCTest
+@testable import FunctionalMarvel
+
+class FunctionalMarvelTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
